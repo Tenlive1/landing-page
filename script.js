e6082db5d8fac0947cba2d87c15f81a47a47e609
Perry <93749717+Tenlive1@users.noreply.github.com>
2023-06-06 23:59:25 -0500

    done with the hamburger menu

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 window.onload = function(){
     const menue_btn = document.querySelector('.hamburger');
-
+    const mobile_menu = document.querySelector('.mobile-nav');
     menue_btn.addEventListener('click',()=>{
-        menue_btn.classList.toggle('is-active')
+        menue_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
     })
+
 }
