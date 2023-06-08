80d84e03970d9aeca7f707a697558586129537e9
Perry <93749717+Tenlive1@users.noreply.github.com>
2023-06-08 01:42:40 -0500

    done with the first layer

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 window.onload = function(){
     const menue_btn = document.querySelector('.hamburger');
     const mobile_menu = document.querySelector('.mobile-nav');
+
+    const listen_btn = document.querySelector('.Listen-btn');
     menue_btn.addEventListener('click',()=>{
         menue_btn.classList.toggle('is-active');
         mobile_menu.classList.toggle('is-active');
     })
+
+    listen_btn.addEventListener('click',()=>{
+        let val = Math.floor(Math.random() * 2);
+        if(val == 0){
+            window.open("https://www.youtube.com/watch?v=IIHoq68i5f0&ab_channel=SonLe","_blank"); 
+        }else if(val == 1){
+            window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley","_blank"); 
+        }
+        console.log(val)
+       
+    });
 
 }
