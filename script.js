238e0cca676bfdaae558f4166f51eee3fd5091fc
Perry <93749717+Tenlive1@users.noreply.github.com>
2023-06-13 23:20:48 -0500

    done with layer-2

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.onload = function(){
     const mobile_menu = document.querySelector('.mobile-nav');
 
     const listen_btn = document.querySelector('.Listen-btn');
+    const idol_btn = document.querySelector('.num-1');
     menue_btn.addEventListener('click',()=>{
         menue_btn.classList.toggle('is-active');
         mobile_menu.classList.toggle('is-active');
@@ -16,6 +17,14 @@ window.onload = function(){
             window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley","_blank"); 
         }
     });
+    idol_btn.addEventListener('click',()=>{
+      let val = Math.floor(Math.random() * 2);
+      if(val == 0){
+        window.open("https://www.youtube.com/watch?v=ZRtdQ81jPUQ&ab_channel=Ayase%2FYOASOBI","_blank"); 
+    }else if(val == 1){
+        window.open("https://www.billboard.com/music/chart-beat/yoasobi-idol-tops-billboard-global-excl-us-chart-1235346245/","_blank"); 
+    }
+    })
 
     var swiper = new Swiper(".slide-content", { 
       slidesPerView: "1",
