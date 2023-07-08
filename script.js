c2a0327a9f63cb15af5d24c0712888a4099b4e82
Perry <93749717+Tenlive1@users.noreply.github.com>
2023-07-08 01:20:07 -0500

    deleted the test code and wrote a comment

--- script.js
+++ script.js
@@ -9,9 +9,8 @@ window.onload = function(){
         mobile_menu.classList.toggle('is-active');
     });
 
-    const home_btn = document.querySelector('.home-btn');
+    const home_btn = document.querySelector('.home-btn');// when user click home it will close the menue
     home_btn.addEventListener('click',()=>{
-      console.log("click");
       menue_btn.classList.toggle('is-active');
       mobile_menu.classList.toggle('is-active');
   });
